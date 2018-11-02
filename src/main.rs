8faa62b2a3614926161f8e988e4fc1c2e4072815
ToRainu256 <torainu256@gmail.com>
2018-11-02 15:49:48 +0900

    ｘ座標の計算が完成

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 extern crate exec;
 
-fn get_x(latitude: f64, zoom_lev: f64){
+fn get_x(latitude: f64, zoom_lev: f64) -> f64{
 
-    let x = 2.0f64.powf( zoom_lev+7.0)*(latitude/180.0 + 1.0);
+      2.0f64.powf( zoom_lev+7.0)*(latitude/180.0 + 1.0)
     
 
 }
 
-fn get_y(longtitude: f64, zoom_lev: f64){
-    let y = 2.0f64.powf( zoom_lev+7.0)*std::f64::consts::FRAC_1_PI*(-1.0*(0.5*((1.0+longtitude)/(1.0-longtitude))f64.log());
-}
+//fn get_y(longtitude: f64, zoom_lev: f64){
+//    let y = 2.0f64.powf( zoom_lev+7.0)*std::f64::consts::FRAC_1_PI*(-1.0*(0.5*));
+//}
 
 
 pub fn main() {
 
-    let get_https = exec::Command::new("curl")
-        .arg("-o")
-        .arg("favicon.ico")
-        .arg("https://kosenfesmap.maizuru-ct.ac.jp/favicon.ico")
-        .exec();
+    //let get_https = exec::Command::new("curl")
+    //    .arg("-o")
+    //    .arg("favicon.ico")
+    //    .arg("https://kosenfesmap.maizuru-ct.ac.jp/favicon.ico")
+    //    .exec();
 
-    println!("{}", get_https);
+    println!("{}", get_x(32.5, 13.0));
 }
 
